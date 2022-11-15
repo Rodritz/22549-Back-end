b6024cca9aa288858394c0c31e7153c77346ab2a
Rodritz <rodrigourq@gmail.com>
2022-11-15 20:07:46 -0300

    add . interfaces

--- src/main/java/ar/com/codoacodo/buscador/HomeMain.java
+++ src/main/java/ar/com/codoacodo/buscador/HomeMain.java
@@ -3,16 +3,18 @@ package ar.com.codoacodo.buscador;
 public class HomeMain {
 
 	public static void main(String[] args) {
-		// para crear un objeto de una clase x
-		//Clase nombreDelObjeto = new Clase();
+		//1- para crear un objeto de una clase xx 
+		//Clase nombreDelObjeto = new Clase(); (esto seria instanciar el objeto)
 		Buscador miBuscador = new Buscador();
 		//ahora  mi buscador esta vivo! entonces puedo 
-		//ejectar cualquiera de sus metodos "publicos"
+		//ejecutar cualquiera de sus metodos "publicos"
 		
-		//primero defino cual es la clave de busqueda
+		//2-primero defino cual es la clave de busqueda ejecutando un metodo
+		//nombreDelObjeto.metodo();
 		miBuscador.cambiarClave("Iron man");
 		
-		//ejecuto el metodo buscar
+		//3-para ejecutar un metodo del objeto
+		//nombreDelObjeto.metodo();
 		miBuscador.buscar();
 		
 		//mostrar cuantos resultados encontro el buscador
