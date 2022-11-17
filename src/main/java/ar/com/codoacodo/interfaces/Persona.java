691e0cc6cfdf2d562f7abf595681c69ac4b0e5ec
Rodritz <rodrigourq@gmail.com>
2022-11-16 21:51:52 -0300

    git add . repaso

--- src/main/java/ar/com/codoacodo/interfaces/Persona.java
+++ src/main/java/ar/com/codoacodo/interfaces/Persona.java
@@ -1,17 +1,24 @@
 package ar.com.codoacodo.interfaces;
 
 public class Persona {
-
+	//atributos
 	private String nombre;
 	private String idioma;//nace
 	private String[] otrosIdiomas;//aprende
 	
+	//constructor
 	public Persona(String nombre, String idioma) {
-		setNombre(nombre);
-		setIdioma(idioma);
-		setOtrosIdiomas(new String[0]);
+		setNombre(nombre);//this.nombre = nombre;//this.nombre hace referencia 
+											//al atributo interno de la clase
+											//mientras que nombre hace referencia al 
+											//parametro, que viene por el objeto instanciado
+		setIdioma(idioma);//this.idioma = idioma;
+		setOtrosIdiomas(new String[0]);//this otroIdiomas = new String[]{}; tambien se puede decir new String [0];
+		//el constructor no altera los atributos sino que utiliza los 
+		//metodos para hacerlo. 
 	}
 	
+	//metodos
 	private void setOtrosIdiomas(String[] idiomas) {
 		this.otrosIdiomas = idiomas; 
 	}
@@ -20,10 +27,11 @@ public class Persona {
 		System.out.println(getNombre() + " dice " + palabra + " en " + getIdioma());
 	}
 	
-	//sobrecarga de metodos: una forma de polimorfismo
+	//sobrecarga de metodos:es una forma de polimorfismo (es cuando hay 2 metodos 
+	//con el mismo nombre pero diferentes parametros
 	public void decir(String algo, String idioma) {
-		// si this.idomas es igual al idioma 
-		// o si el idoma esta en el array de otros idiomas
+		// si this.idioma es igual al idioma 
+		// o si el idioma esta en el array de otros idiomas
 		// entonces puede decir algo en ese idioma
 		
 		//en java no se usa el == para comparar objetos, sino el .equals()
@@ -35,7 +43,8 @@ public class Persona {
 		}
 		
 	}
-	
+	//creamos un metodo privado para validar por medio del metodo existe
+	//si el idioma es nativo o esta dentro del array otrosIdiomas
 	private boolean existe(String idioma) {
 		//"siempre" usar los getter  
 		boolean existe = false;
@@ -45,16 +54,19 @@ public class Persona {
 		return existe;
 	}
 	
-	public void aprender(String nuevoIdioma) {//chino?
-		// si el dioma nativo NO es chino,
-		// && this.otrosIdiomas no existe chino
+	public void aprender(String nuevoIdioma) {//portuñol?
+		// si el dioma nativo NO es portuñol,
+		// && this.otrosIdiomas no existe portuñol
 		// agrego a la lista de this.otrosIdiomas 
 		if (!getIdioma().equals(nuevoIdioma) && !existe(nuevoIdioma)) {
+			//construyo un nuevo vector del largo del vector otrosIdiomas + 1
 			String[] nuevosIdiomas = new String[this.otrosIdiomas.length + 1];
-			for(int i=0;i<this.otrosIdiomas.length;i++) {//f6
-				nuevosIdiomas[i] = this.otrosIdiomas[i];		
+			//recorro el vector.Un ej seria ==>for(String nuevosIdiomas : this.otrosIdiomas)
+			for(int i=0;i<this.otrosIdiomas.length;i++) {
+				nuevosIdiomas[i] = this.otrosIdiomas[i];
 			}
 			nuevosIdiomas[this.otrosIdiomas.length] = nuevoIdioma;
+			//ahora con el setter agrego el nuevo idioma y reemplazo OtrosIdiomas
 			setOtrosIdiomas(nuevosIdiomas);
 		}
 	}
@@ -73,7 +85,6 @@ public class Persona {
 		if(nombre == null) {
 			nombre = "";
 		}
-		
 		this.nombre = nombre;
 	}
 
@@ -85,6 +96,8 @@ public class Persona {
 		return otrosIdiomas;
 	}
 	
-	//alt+shit+s
+	//alt+shit+s crea los getters y setters
+	//los getters devuelven informacion y son publicos
 	
-}
+	
+}   //con f5 me meto dentro del metodo en el debug
