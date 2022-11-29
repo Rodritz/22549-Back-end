5614aefe270b3e86ed6ef9c8402da48df8c21575
Rodritz <rodrigourq@gmail.com>
2022-11-29 20:18:54 -0300

    add. conexion a db

--- src/main/java/ar/com/codoacodo/dao/IArticuloDAO.java
+++ src/main/java/ar/com/codoacodo/dao/IArticuloDAO.java
@@ -1,10 +1,10 @@
 package ar.com.codoacodo.dao;
 
-public interface IArticuloDAO {
+import ar.com.codoacodo.domain.Articulo;
+
+public interface IArticuloDAO  {
 	
-	public void guardar (Articulo a);//entity:Articulo?
-	public void actualizar (Articulo a);//entity:Articulo?
-	public void eliminar (Integer id);
-	public void buscarPorId (Integer id);
+	public Articulo getById (Long id) throws Exception;
+	
 
 }
